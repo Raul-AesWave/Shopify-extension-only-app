d25d9ef9be015bc5c08f7f1ad2bb1732b5ac740e
Raul-AesWave <raul@aeswave.com>
2025-05-21 16:04:23 -0700

    Updated to function for both templates(1ft-20ft, and 6ft-15) Still Needs to be updated to work with other discounts

--- extensions/custom-wire-pricing-based-on-length/src/run.rs
+++ extensions/custom-wire-pricing-based-on-length/src/run.rs
@@ -4,32 +4,33 @@ use shopify_function::Result;
 #[shopify_function_target(query_path = "src/run.graphql", schema_path = "schema.graphql")]
 fn run(input: input::ResponseData) -> Result<output::FunctionRunResult> {
     let mut discounts = vec![];
-
+    
     for line in input.cart.lines.iter() {
-        if let Some(custom_length) = &line.custom_length {
-            if let Some(value_str) = &custom_length.value {
-                if let Ok(length) = value_str.parse::<f64>() {
-                    let discount_feet = if length < 20.0 {
-                        20 - length.trunc() as i64
-                    } else {
-                        0
-                    };
+        let default_price = line.cost.subtotal_amount.amount.to_string().parse::<f64>()?;
+        
+        if let Some(price_change) = &line.price_change {
+            
+            if let Some(price_change_value) = &price_change.value {
                 
-                    let amount = Decimal::from(discount_feet as f64); // $X.00 off
-                
-                    discounts.push(output::Discount {
-                        message: Some(format!("{} ft selected", length)),
-                        targets: vec![output::Target::CartLine(output::CartLineTarget {
-                            id: line.id.to_string(),
-                            quantity: None,
-                        })],
-                        value: output::Value::FixedAmount(output::FixedAmount {
-                            amount,
-                            applies_to_each_item: Some(false),
-                        }),
-                    });
+                if let Ok(new_price) = price_change_value.parse::<f64>() {
+                    
+                    if default_price != new_price {
+                        let discount_amount = default_price - new_price;
+
+                        discounts.push(output::Discount {
+                            message: Some(format!("${}.00 off", discount_amount.trunc() as i64)),
+                            targets: vec![output::Target::CartLine(output::CartLineTarget {
+                                id: line.id.to_string(),
+                                quantity: None,
+                            })],
+                            value: output::Value::FixedAmount(output::FixedAmount {
+                                amount: Decimal::from(discount_amount as f64),
+
+                                applies_to_each_item: Some(false),
+                            }),
+                        });
+                    }
                 }
-                
             }
         }
     }
